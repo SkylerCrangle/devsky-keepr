67cc3f77c4b54fd3d281a630d7ce832ad801ee04
SkylerCrangle <smcrangle@gmail.com>
2020-04-06 10:30:25 -0600

    first commit

---
+++ Repositories/KeepsRepository.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Keepr.Models;
+using Dapper;
+
+namespace Keepr.Repositories
+{
+  public class KeepsRepository
+  {
+    private readonly IDbConnection _db;
+
+    public KeepsRepository(IDbConnection db)
+    {
+      _db = db;
+    }
+
+    internal IEnumerable<Keep> Get()
+    {
+      string sql = "SELECT * FROM Keeps WHERE isPrivate = 0;";
+      return _db.Query<Keep>(sql);
+    }
+
+    internal Keep Create(Keep KeepData)
+    {
+      string sql = @"
+            INSERT INTO keeps 
+            (name, description, userId, img, isPrivate, views, shares, keeps) 
+            VALUES 
+            (@Name, @Description, @UserId, @Img, @IsPrivate, @Views, @Shares, @Keeps);
+            SELECT LAST_INSERT_ID()";
+      KeepData.Id = _db.ExecuteScalar<int>(sql, KeepData);
+
+      return KeepData;
+    }
+  }
+}
---
+++ wwwsrc/src/authConfig.js
@@ -0,0 +1,3 @@
+export const domain = "dev-sky.auth0.com";
+export const audience = "https://devsky.com";
+export const clientId = "h1Ej8tWtDwbUe04B1teI35n54TvxVWC0";
